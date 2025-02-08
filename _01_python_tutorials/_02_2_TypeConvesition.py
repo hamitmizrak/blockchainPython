34217e5462392989ed3fc55f7eac2c3e4c8cdff5
Hamit Mizrak <hamitmizrak@gmail.com>
2025-02-08 09:53:10 +0300

    user data

---
+++ _01_python_tutorials/_02_2_TypeConvesition.py
@@ -0,0 +1,6 @@
+#####################################################################################
+#### Type Convesition ###############################################################
+sayi1= input("1.sayıyı giriniz\n")
+sayi2= input('2.sayıyı giriniz\n')
+toplam=int(sayi1)+int(sayi2); # int(), float(), str(),
+print("toplam sonuc",toplam)
