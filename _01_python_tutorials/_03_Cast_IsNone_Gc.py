469bb6c1dfd85e47c378c8ad7bb225210f28c7ea
Hamit Mizrak <hamitmizrak@gmail.com>
2025-02-08 09:53:34 +0300

    is none,cast

---
+++ _01_python_tutorials/_03_Cast_IsNone_Gc.py
@@ -0,0 +1,64 @@
+###################################################################
+# Cast: Type Casting yani Değişkenlerin Tip dönüşümlerini kullanmak
+# int()          : Tam Sayıya çevirir.
+# float()        : Virgüllü Sayıya çevirir.
+# str()          : kelime  çevirir.
+# bool()         : Boolean çevirir.
+
+number2 = "44"
+print(number2)
+print("string:\t ", type(number2))
+
+# int() ile string'i tam sayıya çevirebiliriz.'
+number2=int(number2)
+print("integer: ", type(number2))
+
+# float() ile string'i virgüllü sayıya çevirebiliriz.'
+number2=float(number2)
+print("float:\t ", type(number2))
+
+################################################################################
+# Escape Character: Kaçış karekteri
+print("1.satır\n2.satır")
+print("3.satır\n\t4.satır")
+print("5.satır\n\r\t6.satır")
+
+# Unicode Karakter 16bit(u) ve 32bit(U)
+print("\u0001")
+print("\U0001F600")
+
+# Dosya Yollarına
+print("C:\\User\\AppData")
+print(r"C:\User\AppData")  # Ham String: (r) Kaçış karakterini devre dışı bırakmak için kullanıyoruz.
+
+#####################################################################################
+#### None ###########################################################################
+# None: Python'da None özel bir veri türüdür ve boş veya tanımsız bir değeri ifade eder.
+data = None  # Tanımsız veya boş değeri temsil eder
+print("boş değer: ", data)
+
+#  is None: None'un aynı nesne olup olmadığını kontrol eder.
+#  == None: None'a eşit olup olmadığını kontrol eder.
+
+"""
+x = None
+
+#Doğru kullanım
+if x is None:
+    print("x gerçekten None")
+
+# Yanlış olmasa da önerilmeyen kullanım
+if x == None:
+    print("x None'a eşit")
+"""
+################################################################
+# GC (Garbarage Collector)
+"""Python'da değişkenler bellek yönetimi açısında otomatik olarak işlenir """
+# GC: Gereksiz hale gelmiş ve kullanılmayan çöp olan nesleri otomatik olarak siler.
+number1 = 44
+# Buradaki number1=44 olan ver artık bir çöptür çünkü alt satıda aynı isimle başka bir veri vardır
+number1 = 23
+
+
+
+
